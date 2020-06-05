f95a5843df2d6f16539e5874a265ac55ba5f1138
jerodrigug <28200666+jerodrigug@users.noreply.github.com>
2020-06-04 23:18:57 -0500

    cambiando direcciones del back

--- frontend/seat/src/app/login.service.ts
+++ frontend/seat/src/app/login.service.ts
@@ -7,10 +7,10 @@ import { HttpClient } from '@angular/common/http';
 export class LoginService {
   constructor(private httpClient:HttpClient) { }
 
-  getDocumentsUrl="http://127.0.0.1:5000/documents"
-  loginUrl="http://127.0.0.1:5000/login"
-  uploadDocUrl="http://127.0.0.1:5000/upload_unauthenticated_document"
-  verificarDocumentoUrl="http://127.0.0.1:5000/authenticate_document"
+  getDocumentsUrl="http://ec2-100-26-223-58.compute-1.amazonaws.com/documents"
+  loginUrl="http://ec2-100-26-223-58.compute-1.amazonaws.com//login"
+  uploadDocUrl="http://ec2-100-26-223-58.compute-1.amazonaws.com//upload_unauthenticated_document"
+  verificarDocumentoUrl="http://ec2-100-26-223-58.compute-1.amazonaws.com//authenticate_document"
 
   changeLoginStatus(loginForm){
      return this.httpClient.post(this.loginUrl,loginForm)
